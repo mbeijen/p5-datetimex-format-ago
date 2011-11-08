0976e979c867dbaf66fbb4eaea71e9e0e06b25ad
Toby Inkster <mail@tobyinkster.co.uk>
2011-11-08 15:19:19 +0000

    Add FR, PT, ES translations; document that BCP47 language codes should be used; add German language test cases; prepare for release.
    --HG--
    branch : DateTimeX-Format-Ago
    extra : convert_revision : svn%3A3fe8e991-6959-4966-b76d-b07eca2b6e37/DateTimeX-Format-Ago%401048

--- lib/DateTimeX/Format/Ago.pm
+++ lib/DateTimeX/Format/Ago.pm
@@ -12,7 +12,7 @@ BEGIN {
 
 use Carp 0 qw[];
 use DateTime 0 ;
-use Scalar::Util 0 qw[blessed refaddr];
+use Scalar::Util 0 qw[blessed];
 
 our %__;
 BEGIN {
@@ -36,6 +36,48 @@ BEGIN {
 		hours     => ["vor %d Stunden", "vor einer Stunde"],
 		minutes   => ["vor %d Minuten", "vor einer Minute"],
 		};
+	
+	# These courtesy of Google Translate...
+	$__{FR} = {
+		future    => "à l'avenir",
+		recent    => "viens",
+		years     => ["il ya %d ans", "il ya un an"],
+		months    => ["il ya %d mois", "il ya un mois"],
+		weeks     => ["il ya %d semaines", "il ya une semaine"],
+		days      => ["il ya %d jours", "il ya un jour"],
+		hours     => ["il ya %d heures", "il ya une heure"],
+		minutes   => ["il ya %d minutes", "il ya une minute"],
+		};
+	$__{ES} = {
+		future    => "en el futuro",
+		recent    => "ahora mismo",
+		years     => ["hace %d años", "hace un año"],
+		months    => ["hace %d meses", "hace un mes"],
+		weeks     => ["hace %d semanas", "hace una semana"],
+		days      => ["hace %d días", "hace un día"],
+		hours     => ["hace %d horas", "hace una hora"],
+		minutes   => ["hace %d minutos", "hace un minuto"],
+		};
+	$__{PT} = {
+		future    => "no futuro",
+		recent    => "só agora",
+		years     => ["%d anos atrás", "há um ano"],
+		months    => ["%d meses atrás", "há um mês"],
+		weeks     => ["%d semanas atrás", "há uma semana"],
+		days      => ["%d days atrás", "há um dia"],
+		hours     => ["%d horas atrás", "há uma hora"],
+		minutes   => ["%d minutos atrás", "há um minuto"],
+		};
+
+	# ISO 639-2
+	$__{ENG} = $__{EN};
+	$__{FRE} = $__{FR};
+	$__{FRA} = $__{FR};
+	$__{ENG} = $__{EN};
+	$__{GER} = $__{DE};
+	$__{DEU} = $__{DE};
+	$__{SPA} = $__{ES};
+	$__{POR} = $__{PT};
 }
 
 sub new
@@ -96,7 +138,7 @@ sub _strings
 		$language =~ s/(^|[_-])([^_-]*)$//;
 	}
 	
-	Carp::croak(sprintf("%s doesn't know about language %s", __PACKAGE__, $self->{language}));
+	Carp::croak(sprintf("%s doesn't know about language '%s'", __PACKAGE__, $self->{language}));
 }
 
 TRUE;
@@ -123,8 +165,13 @@ method?
 
 =item C<< new(language => $lang) >>
 
-Creates a formatter object. If the language is ommitted, extracts it from
-C<< $ENV{LANG} >>.
+Creates a formatter object for the given language (a BCP47 language code).
+If the language is omitted, extracts it from C<< $ENV{LANG} >>.
+
+Decent English ('en') and German ('de') support is provided. French ('fr'),
+Portuguese ('pt') and Castillian Spanish ('es') are also provided, but some
+of the strings were translated with Google Translate, so they might not be
+perfect.
 
 =back
 
@@ -134,7 +181,7 @@ C<< $ENV{LANG} >>.
 
 =item C<< format_datetime($dt) >>
 
-Returns something like "3 days ago" or "just now".
+Returns something like "3 days ago", "just now" or "hace un año".
 
 =item C<< parse_datetime($string) >>
 
@@ -147,12 +194,14 @@ Croaks. Don't use this.
 Please report any bugs to
 L<http://rt.cpan.org/Dist/Display.html?Queue=DateTimeX-Format-Ago>.
 
-I'm actively seeking translations - only have English and German so far.
-Feel free to attach patches for other languages as bug reports.
+I'm actively seeking translations. Feel free to attach patches for other
+languages as bug reports.
 
 =head1 SEE ALSO
 
 L<DateTime>, L<DateTime::Format::Natural>.
+
+L<http://www.rfc-editor.org/rfc/bcp/bcp47.txt>.
 
 =head1 AUTHOR
 
